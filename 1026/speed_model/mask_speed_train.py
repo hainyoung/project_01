dba7c9c8d8ef152e42aa34547c5fc2051dfdc144
hainyoung <pnp3737@gmail.com>
2020-10-26 13:49:06 +0900

    1026

---
+++ 1026/speed_model/mask_speed_train.py
@@ -0,0 +1,51 @@
+import sys
+
+import numpy as np
+import cv2
+
+im = cv2.imread('./speed_model/1022_speed.png')
+#im3 = im.copy()
+
+gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gray,(3,3),0)
+# thresh = cv2.adaptiveThreshold(blur,255,1,1,11,2)
+
+lower_white = np.array([136])
+upper_white = np.array([255])
+mask_white = cv2.inRange(blur,lower_white, upper_white)
+mask_white = cv2.bitwise_and(blur, blur, mask = mask_white)
+
+#_, thresh = cv2.threshold(mask_green,135,255, cv2.THRESH_OTSU)
+
+contours,hierarchy = cv2.findContours(mask_white,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+
+cv2.imshow("mask", mask_white)
+# cv2.imshow("blur", blur)
+samples =  np.empty((0,100))
+responses = []
+keys = [i for i in range(48,58)]
+
+for cnt in contours:
+    if cv2.contourArea(cnt) > 10 :
+        [x,y,w,h] = cv2.boundingRect(cnt)
+
+        if  h>25:
+            cv2.rectangle(im,(x,y),(x+w,y+h),(0,0,255),2)
+            roi = mask_white[y:y+h,x:x+w]
+            roismall = cv2.resize(roi,(10,10))
+            cv2.imshow('norm',im)
+            key = cv2.waitKey(0)
+
+            if key == 27:  # (escape to quit)
+                sys.exit()
+            elif key in keys:
+                responses.append(int(chr(key)))
+                sample = roismall.reshape((1,100))
+                samples = np.append(samples,sample,0)
+
+responses = np.array(responses,np.float32)
+responses = responses.reshape((responses.size,1))
+print("training complete")
+
+np.savetxt('./mask_speed_generalsamples.data',samples)
+np.savetxt('./mask_speed_generalresponses.data',responses)
---
+++ 1026/test.py
@@ -0,0 +1,135 @@
+import numpy as np
+import cv2
+import sys
+
+# sys.path.append("C:\\Users\\Fazliddin\\Desktop\\FINAL\\functions.py") 
+
+# Importing the functions 
+from functions import *
+
+#Videos from 1015  STEP 3
+# cap = cv2.VideoCapture("./1015_video_list/dim_10_10frame.mp4")
+# cap = cv2.VideoCapture("./1015_video_list/dim_10_16frame.mp4")
+# cap = cv2.VideoCapture("./1015_video_list/dim_10_20frame.mp4")
+# cap = cv2.VideoCapture("./1015_video_list/dim_10_fullframe.mp4")
+# cap = cv2.VideoCapture("./1015_video_list/indoor_10_22frame.mp4")
+# cap = cv2.VideoCapture("./1015_video_list/indoor_12_22frame.mp4")
+# cap = cv2.VideoCapture("./1015_video_list/indoor_15_22frame.mp4")
+# cap = cv2.VideoCapture("./1015_video_list/outdoor_8_22frame.mp4")
+# cap = cv2.VideoCapture("./1015_video_list/outdoor_12_22frame.mp4")
+# cap = cv2.VideoCapture("./1015_video_list/outdoor_12_brightness_max_22frame.mp4")
+# cap = cv2.VideoCapture("./1015_video_list/outdoor_12_saturation_max_22frame.mp4")
+# cap = cv2.VideoCapture("./1015_video_list/standard_15_22frame.mp4")
+
+# 1023
+cap = cv2.VideoCapture("./1023_video_list/1023_cut_video.mp4")
+
+
+c = 1
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    if c%2==0:
+
+        # ROI: DATE and TIME
+        x_1,y_1,h_1,w_1 = 18,41,90,863
+        rec_1 = draw_rec(frame,"Current Time", x_1,y_1,h_1,w_1) 
+        dt = date_time_recognizer(frame,28,51,80,845)
+        # dt = date_time_recognizer(frame,27,50,69,749) # 1023 video
+                   
+        date_time = "".join(dt)
+        date_time = date_time[::-1]
+        # print(date_time[:4] + "-" + date_time[4:6] + "-" + date_time[6:8], end = ",")
+        # print(date_time[8:10] + ":" + date_time[10:12] + ":" + date_time[12:14], end = ",")
+        print("date:" + date_time[:4] + "-" + date_time[4:6] + "-" + date_time[6:8], end = ", ")
+        print("time:" + date_time[8:10] + ":" + date_time[10:12] + ":" + date_time[12:14], end = ", ")
+
+        # ROI: SPEED
+        #y_2,h_2,x_2,w_2 = 524,89,1146,140 cut video
+        #roi_date_time =frame[y_1:y_1+h_1,x_1:x_1+w_1] cut video
+        
+        # x_2,y_2,h_2,w_2 = 1029,550,77,136
+        x_2,y_2,h_2,w_2 = 950,490,77,136 # 1023 video
+        rec_2 = draw_rec(frame,"Speed", x_2,y_2,h_2,w_2) 
+        
+        # roi_speed = speed_digit_recognizer(frame,1039,560,57,96) # thresh O / mask X
+        # roi_speed = mask_speed_digit_recognizer(frame,1039,560,57,96) # thresh X / mask O
+        
+        # roi_speed = speed_digit_recognizer(frame,960,500,56,88) # 1023 video
+        roi_speed = mask_speed_digit_recognizer(frame,960,500,56,88) # 1023 video
+
+        #roi_speed = speed_digit_recognizer(frame,1146,524,89,140) cut video
+        speed = "".join(roi_speed)
+        speed = speed[::-1]
+
+        # speed = int(speed)
+        # print(type(speed))
+        if not speed:
+            print("speed:", end = ',')
+
+        elif int(speed) > 180:
+            print("speed:", end = ',')
+
+        else:
+            print("speed:" + speed, end = ", ")
+
+        # ROI: LANE
+        # y_3,h_3,x_3,w_3 = 450,43,933,52 # 1015 video
+        y_3,h_3,x_3,w_3 = 393,29,860,44 # 1023 video
+        
+        roi_4 = frame[y_3:y_3+h_3,x_3:x_3+w_3]
+        hsv_green_lane = hsv_green(roi_4)
+        hsv_white_lane = hsv_white(roi_4)
+        rec_3 = draw_rec(frame,"LDWS", x_3,y_3,h_3,w_3)
+
+        hsv_green_lane_per_row = np.average(hsv_green_lane, axis = 0)
+        avg_green_lane_ = np.average(hsv_green_lane_per_row, axis = 0)
+        avg_green_lane = np.average(avg_green_lane_)
+
+        hsv_white_lane_per_row = np.average(hsv_white_lane, axis = 0)
+        avg_white_lane_ = np.average(hsv_white_lane_per_row, axis = 0)
+        avg_white_lane = np.average(avg_white_lane_)
+
+        # # FIFTH ROI : AUTO
+        # y_4,h_4,x_4,w_4 = 459, 40, 1217, 51 # 1015 video
+        y_4,h_4,x_4,w_4 = 400,33,1153,36 # 1023 video
+        
+        roi_5 = frame[y_4:y_4+h_4,x_4:x_4+w_4]
+        hsv_green_auto = hsv_green(roi_5)
+        hsv_white_auto = hsv_white(roi_5)
+        rec_4 = draw_rec(frame,"LKAS", x_4,y_4,h_4,w_4)
+
+        hsv_green_auto_per_row = np.average(hsv_green_auto, axis = 0)
+        avg_green_auto_ = np.average(hsv_green_auto_per_row, axis = 0)
+        avg_green_auto = np.average(avg_green_auto_)
+
+        hsv_white_auto_per_row = np.average(hsv_white_auto, axis = 0)
+        avg_white_auto_ = np.average(hsv_white_auto_per_row, axis = 0)
+        avg_white_auto = np.average(avg_white_auto_)
+         
+        # lane
+        if avg_green_lane > 0 :
+            print("LDWS:" + "1", end = ', ', sep = ',')
+        else : 
+            print("LDWS:" + "0", end = ', ', sep = ',')
+
+        # auto       
+        if avg_green_auto > 0 or avg_white_auto > 0:
+            print("LKAS:" + "1")
+        else :
+            print("LKAS:" + "0")
+
+        if(cv2.waitKey(1) & 0xFF == ord('q')):
+            break
+        #sys.stdout = open('OTSU_outdoor_12_22frame.txt', 'a', -1, 'utf-8')     
+        #Show all the windows
+        cv2.imshow("Frame", frame)    
+        cv2.moveWindow("Frame",0, 0)
+        
+    c+=1
+cap.release()
+# out.release()
+cv2.destroyAllWindows()
